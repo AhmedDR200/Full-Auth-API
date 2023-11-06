f8d79083b348940c948e9573080b48b8d0c92511
AhmedDR200 <alshwwhy212@gmail.com>
2023-11-06 14:39:51 +0200

    Update user model and serializers

--- project/settings.py
+++ project/settings.py
@@ -181,6 +181,8 @@ DJOSER = {
     'USERNAME_RESET_CONFIRM_URL':'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL':'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL':True,
+    'SOCIAL_AUTH_TOKEN_STRATEGY':'djoser.social.token.jwt.TokenStrategy',
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URLS': ['http://localhost:8000'],
     'SERIALIZERS':{
         'user_create': 'accounts.serializers.UserCreateSerializer',
         'user': 'accounts.serializers.UserCreateSerializer',
